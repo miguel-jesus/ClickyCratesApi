46cec56976308acaa15019a1e0e5d3c1d42b02cf
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-05-29 19:59:19 +0200

    Añadido el script de la tabla Players

---
+++ ClickyCratesApi/TableScripts/dbo.Players.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Players] (
+    [Id]        NVARCHAR (128) NOT NULL,
+    [FirstName] NVARCHAR (128) NULL,
+    [LastName]  NVARCHAR (128) NULL,
+    [NickName]  NVARCHAR (128) NULL,
+    [City]      NVARCHAR (128) NULL,
+    [BirthDay]  NVARCHAR (128) NULL,
+    [IsOnline]  BIT            DEFAULT ((0)) NOT NULL,
+    [LastLogin] NVARCHAR (128) NULL,
+    CONSTRAINT [FK_dbo.Players] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Players_AspNetUsers] FOREIGN KEY ([Id]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
