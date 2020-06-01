4a41c7d8c97898a4b55b0b049a934bb6b18638d4
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-06-01 13:36:46 +0200

    Añadimos el Script de la tabla Objects

---
+++ ClickyCratesApi/TableScripts/dbo.Objects.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Objects]
+(
+	[Id]        NVARCHAR (128) NOT NULL,
+    [Synti] NVARCHAR (128) NULL DEFAULT 0,
+    [Box]  NVARCHAR (128) NULL DEFAULT 0,
+    [Barrel]  NVARCHAR (128) NULL DEFAULT 0,
+    [Skull]      NVARCHAR (128) NULL DEFAULT 0,
+    CONSTRAINT [FK_dbo.Objects] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Objects_AspNetUsers] FOREIGN KEY ([Id]) REFERENCES [dbo].[AspNetUsers] ([Id])
+)
