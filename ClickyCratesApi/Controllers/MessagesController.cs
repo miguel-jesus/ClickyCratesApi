4d575c0c6a60415b4f4579e6b2d884cfc5b77856
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-06-08 01:00:02 +0200

    Como en todo primero creamos un modelo exacto a la base de datos y luego creamos un controlador, en este caso con 2 endPoint. Creamos un Get para recuperar una lista de mensajes y un Post para insertar los mensajes que el unico campo que no insertaremos de la tabla mensajes será el Id de la tabla ya que lo haremos autoincremt ,ya que en este caso el Id del Player no podía ser la clave primaria

---
+++ ClickyCratesApi/Controllers/MessagesController.cs
@@ -0,0 +1,55 @@
+﻿using ClickyCratesApi.Models;
+using Dapper;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ClickyCratesApi.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Messages")]
+    public class MessagesController : ApiController
+    {
+        //POST api/Messages/InsertNewMessage
+        [HostAuthentication(DefaultAuthenticationTypes.ExternalBearer)]
+        [HttpPost]
+        [Route("InsertNewMessage")]
+        public bool InsertNewPlayer(MessagesModel messagesModel)
+        {
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = $"INSERT INTO dbo.Messages(IdPlayer,Messages,MessageHour)" +
+                    $"VALUES ('{messagesModel.IdPlayer}','{messagesModel.Messages}','{messagesModel.MessageHour}')";
+                int rows = cnn.Execute(sql);
+                if (rows != 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        //Get api/Messages/GetMessages
+        [HttpGet]
+        [Route("GetMessages")]
+        public List<MessagesModel> GetMessages()
+        {
+            string authenticatedAspNetUserId = RequestContext.Principal.Identity.GetUserId();
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = $"SELECT IdPlayer,Messages,MessageHour FROM dbo.Messages ";
+
+                List<MessagesModel> messages = cnn.Query<MessagesModel>(sql).ToList();
+                return messages;
+            }
+        }
+    }
+}
---
+++ ClickyCratesApi/Models/MessagesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClickyCratesApi.Models
+{
+    public class MessagesModel
+    {
+        public string IdPlayer { get; set; }
+        public string Messages { get; set; }
+        public string MessageHour { get; set; }
+    }
+}
