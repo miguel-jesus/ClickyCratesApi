f770789006ed365dc740642b622926367537d670
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-06-09 16:59:59 +0200

    subida de los scripts de las tablas actualizadas

---
+++ ClickyCratesApi/TableScripts/dbo.Messages.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Messages] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [IdPlayer]    NVARCHAR (128) NOT NULL,
+    [Messages]    TEXT           NULL,
+    [MessageHour] NVARCHAR (128) NULL,
+    CONSTRAINT [FK_dbo.Messages] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_dbo.Messages_AspNetUsers] FOREIGN KEY ([IdPlayer]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
--- ClickyCratesApi/TableScripts/dbo.Players.sql
+++ ClickyCratesApi/TableScripts/dbo.Players.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[Players] (
-    [Id]        NVARCHAR (128) NOT NULL,
-    [FirstName] NVARCHAR (128) NULL,
-    [LastName]  NVARCHAR (128) NULL,
-    [NickName]  NVARCHAR (128) NULL,
-    [City]      NVARCHAR (128) NULL,
-    [BirthDay]  NVARCHAR (128) NULL,
-    [IsOnline]  BIT            DEFAULT ((0)) NOT NULL,
-    [LastLogin] NVARCHAR (128) NULL,
+    [Id]            NVARCHAR (128) NOT NULL,
+    [FirstName]     NVARCHAR (128) NULL,
+    [LastName]      NVARCHAR (128) NULL,
+    [NickName]      NVARCHAR (128) NULL,
+    [City]          NVARCHAR (128) NULL,
+    [BirthDay]      NVARCHAR (128) NULL,
+    [IsOnline]      BIT            DEFAULT ((0)) NOT NULL,
+    [LastLogin]     NVARCHAR (128) NULL,
+    [HourGameScene] NVARCHAR (128) NULL,
+    [IsBanned]      BIT            DEFAULT ((0)) NOT NULL,
+    [BannedHour]    NVARCHAR (128) NULL,
     CONSTRAINT [FK_dbo.Players] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_dbo.Players_AspNetUsers] FOREIGN KEY ([Id]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
